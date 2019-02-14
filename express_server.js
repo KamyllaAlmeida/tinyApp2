0cd0393796dadf41ea03a716b58839f14c6673f0
KamyllaAlmeida <kamylla.florencio@gmail.com>
2019-02-14 02:44:39 +0000

    Created ind: index page to show short and longurls, new to shorten long URLs and u/:shortURL route to redirect to the long url

---
+++ express_server.js
@@ -0,0 +1,58 @@
+var express = require("express");
+const bodyParser = require("body-parser");
+var app = express();
+var PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+function generateRandomString() {
+  return Math.random().toString(36).substring(2, 8);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let id = req.params.id;
+  let templateVars = { longURL: urlDatabase[id], shortURL: id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+
+
